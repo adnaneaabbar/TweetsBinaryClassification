6302696ecce22fcc87c475fb1a75af6c4f4132f5
adnaneaabbar <adnaneaabbar@gmail.com>
2021-12-12 22:46:09 +0100

    scaffold

--- main.py
+++ main.py
@@ -11,3 +11,23 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+
+# load
+
+
+# preprocess
+
+
+# extract features
+
+
+# encode
+
+
+# split
+
+
+# train
+
+
+# predict
