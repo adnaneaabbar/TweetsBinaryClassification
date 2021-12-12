767e3d650d875c2d29b880dc4728b58760f56651
adnaneaabbar <adnaneaabbar@gmail.com>
2021-12-12 22:23:06 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
